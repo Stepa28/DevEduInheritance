0c9d2f7c124b2080842bd8d70dca39a0a088a64e
Stepa <cntgf.2010@yandex.ru>
2021-11-22 13:23:30 +0300

    Add Battle

---
+++ DevEduInheritance/Battle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using DevEduInheritance.Creatures;
+
+namespace DevEduInheritance
+{
+    public class Battle
+    {
+        public static void NewBattle<T1,T2>(T1 combatingOne, T2 combatingTwo) 
+            where T1 : Creature, ICombatan
+            where T2 : Creature, ICombatan
+        {
+            Random rnd = new();
+            while (!combatingOne.IsDie && !combatingTwo.IsDie)
+            {
+                Thread.Sleep(1000);
+                int damageA = combatingOne.GetAttack(rnd.Next(0, 11));
+                int damageB = combatingTwo.GetAttack(rnd.Next(0, 11));
+                Console.WriteLine($"{combatingOne.Name} наносит {damageA} {combatingTwo.Name}");
+                combatingTwo.Wounds(damageA);
+                Console.WriteLine($"У {combatingTwo.Name.ToLower()} осталось {combatingTwo.Hp}\n");
+                Console.WriteLine($"{combatingTwo.Name} наносит {damageB} {combatingOne.Name}");
+                combatingOne.Wounds(damageB);
+                Console.WriteLine($"У {combatingOne.Name.ToLower()} осталось {combatingOne.Hp}\n");
+            }
+
+            if (combatingOne.IsDie && combatingTwo.IsDie) { Console.WriteLine("Оба умерли"); }
+            else if (combatingTwo.IsDie)
+                Console.WriteLine($"{combatingOne.Name} Победил!\n");
+            else
+                Console.WriteLine($"{combatingTwo.Name} Победил!\n");
+        }
+    }
+}
